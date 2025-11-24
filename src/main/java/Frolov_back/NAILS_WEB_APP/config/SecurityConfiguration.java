dee15d3b3225b3545252bdce2d2cf709950b3d1a
nikfrol2014 <nikitka_frolov_2014@inbox.ru>
2025-11-24 17:23:54 +0300

    №2 тестовая правка для сервера

--- src/main/java/Frolov_back/NAILS_WEB_APP/config/SecurityConfiguration.java
+++ src/main/java/Frolov_back/NAILS_WEB_APP/config/SecurityConfiguration.java
@@ -42,7 +42,8 @@ public class SecurityConfiguration {
                 .csrf(csrf -> csrf.disable())
                 .cors(Customizer.withDefaults())  // Использует бин corsConfigurationSource
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+//                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
